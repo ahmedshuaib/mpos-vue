9ddf340312c2b77901128c08ba422b37beb44e21
junaeidtech <junaid@techmonstar.com>
2023-06-12 20:39:18 +0600

    modified webpack chunk name

--- src/services/AuthServices.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-class AuthServices{
-    userModal(user){
-        const reponse=axios.post('http://localhost:8080/user/usermodal',user)
-        console.log(reponse.data);
-    }
-}
-export default new AuthServices;
