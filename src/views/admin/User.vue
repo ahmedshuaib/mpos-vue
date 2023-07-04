58c3bcafaac10921899fa651a92fb9ca28e822a1
ahmedshuaib <ahmedshuaib46@gmail.com>
2023-07-04 14:30:51 +0600

    added default folder and dummpy file

--- src/views/admin/User.vue
+++ src/views/admin/User.vue
@@ -2,14 +2,15 @@
     <!-- start design here... -->
     <section class="pro-users__section">
         <div class="section__container">
-            <div class="d-flex align-items-center justify-content-between heading-box flex-wrap gap-2">
+            <div
+                class="d-flex align-items-center justify-content-between heading-box flex-wrap gap-2"
+            >
                 <h4 class="mb-0">User</h4>
-                <router-link :to="{name:'userModal'}">
+                <router-link :to="{ name: 'userModal' }">
                     <button class="user-add-btn" id="user-add-btn">
                         <span class="me-1">+</span>Add
                     </button>
                 </router-link>
-
             </div>
         </div>
     </section>
@@ -21,19 +22,19 @@
                     <!-- dropdown menu -->
                     <keep-alive>
                         <div class="dropdown-btn">
-                        <div class="select d-flex alig-items-center gap-4">
-                            <span class="selected">Export</span>
-                            <img src="../assets/img/drop.svg" alt="">
+                            <div class="select d-flex alig-items-center gap-4">
+                                <span class="selected">Export</span>
+                                <img src="../assets/img/drop.svg" alt="" />
+                            </div>
+                            <ul class="menu">
+                                <li>Copy</li>
+                                <li>Export to CSV</li>
+                                <li class="active">Export to Excel</li>
+                                <li>Export to PDF</li>
+                                <li>Print</li>
+                                <li>Column visibility</li>
+                            </ul>
                         </div>
-                        <ul class="menu">
-                            <li>Copy</li>
-                            <li>Export to CSV</li>
-                            <li class="active">Export to Excel</li>
-                            <li>Export to PDF</li>
-                            <li>Print</li>
-                            <li>Column visibility</li>
-                        </ul>
-                         </div> 
                     </keep-alive>
                 </div>
             </div>
@@ -43,7 +44,7 @@
     <section class="stock__section mt-5">
         <div class="section__container">
             <div class="card border-0 bg-white p-4">
-                <table ref="table4" style="width:100%">
+                <table ref="table4" style="width: 100%">
                     <thead>
                         <tr>
                             <th>Name</th>
@@ -63,14 +64,27 @@
                             <td>$1000</td>
                             <td>
                                 <div class="d-flex align-items-center gap-2">
-                                <router-link :to="{name:'edit',params:{id:'1'}}">
-                                    <button class="pro-edit-btn">
-                                    <img src="../assets/img/edit.svg" alt=""> Edit
-                                </button>
-                                </router-link>
-                                <button class="pro-delete-btn">
-                                    <img src="../assets/img/delete.svg" alt=""> Delete
-                                </button>
+                                    <router-link
+                                        :to="{
+                                            name: 'edit',
+                                            params: { id: '1' },
+                                        }"
+                                    >
+                                        <button class="pro-edit-btn">
+                                            <img
+                                                src="../assets/img/edit.svg"
+                                                alt=""
+                                            />
+                                            Edit
+                                        </button>
+                                    </router-link>
+                                    <button class="pro-delete-btn">
+                                        <img
+                                            src="../assets/img/delete.svg"
+                                            alt=""
+                                        />
+                                        Delete
+                                    </button>
                                 </div>
                             </td>
                         </tr>
@@ -82,14 +96,27 @@
                             <td>$1000</td>
                             <td>
                                 <div class="d-flex align-items-center gap-2">
-                                <router-link :to="{name:'edit',params:{id:'1'}}">
-                                    <button class="pro-edit-btn">
-                                    <img src="../assets/img/edit.svg" alt=""> Edit
-                                </button>
-                                </router-link>
-                                <button class="pro-delete-btn">
-                                    <img src="../assets/img/delete.svg" alt=""> Delete
-                                </button>
+                                    <router-link
+                                        :to="{
+                                            name: 'edit',
+                                            params: { id: '1' },
+                                        }"
+                                    >
+                                        <button class="pro-edit-btn">
+                                            <img
+                                                src="../assets/img/edit.svg"
+                                                alt=""
+                                            />
+                                            Edit
+                                        </button>
+                                    </router-link>
+                                    <button class="pro-delete-btn">
+                                        <img
+                                            src="../assets/img/delete.svg"
+                                            alt=""
+                                        />
+                                        Delete
+                                    </button>
                                 </div>
                             </td>
                         </tr>
@@ -101,14 +128,27 @@
                             <td>$1000</td>
                             <td>
                                 <div class="d-flex align-items-center gap-2">
-                                <router-link :to="{name:'edit',params:{id:'1'}}">
-                                    <button class="pro-edit-btn">
-                                    <img src="../assets/img/edit.svg" alt=""> Edit
-                                </button>
-                                </router-link>
-                                <button class="pro-delete-btn">
-                                    <img src="../assets/img/delete.svg" alt=""> Delete
-                                </button>
+                                    <router-link
+                                        :to="{
+                                            name: 'edit',
+                                            params: { id: '1' },
+                                        }"
+                                    >
+                                        <button class="pro-edit-btn">
+                                            <img
+                                                src="../assets/img/edit.svg"
+                                                alt=""
+                                            />
+                                            Edit
+                                        </button>
+                                    </router-link>
+                                    <button class="pro-delete-btn">
+                                        <img
+                                            src="../assets/img/delete.svg"
+                                            alt=""
+                                        />
+                                        Delete
+                                    </button>
                                 </div>
                             </td>
                         </tr>
@@ -120,14 +160,27 @@
                             <td>$1000</td>
                             <td>
                                 <div class="d-flex align-items-center gap-2">
-                                <router-link :to="{name:'edit',params:{id:'1'}}">
-                                    <button class="pro-edit-btn">
-                                    <img src="../assets/img/edit.svg" alt=""> Edit
-                                </button>
-                                </router-link>
-                                <button class="pro-delete-btn">
-                                    <img src="../assets/img/delete.svg" alt=""> Delete
-                                </button>
+                                    <router-link
+                                        :to="{
+                                            name: 'edit',
+                                            params: { id: '1' },
+                                        }"
+                                    >
+                                        <button class="pro-edit-btn">
+                                            <img
+                                                src="../assets/img/edit.svg"
+                                                alt=""
+                                            />
+                                            Edit
+                                        </button>
+                                    </router-link>
+                                    <button class="pro-delete-btn">
+                                        <img
+                                            src="../assets/img/delete.svg"
+                                            alt=""
+                                        />
+                                        Delete
+                                    </button>
                                 </div>
                             </td>
                         </tr>
@@ -139,14 +192,27 @@
                             <td>$1000</td>
                             <td>
                                 <div class="d-flex align-items-center gap-2">
-                                <router-link :to="{name:'edit',params:{id:'1'}}">
-                                    <button class="pro-edit-btn">
-                                    <img src="../assets/img/edit.svg" alt=""> Edit
-                                </button>
-                                </router-link>
-                                <button class="pro-delete-btn">
-                                    <img src="../assets/img/delete.svg" alt=""> Delete
-                                </button>
+                                    <router-link
+                                        :to="{
+                                            name: 'edit',
+                                            params: { id: '1' },
+                                        }"
+                                    >
+                                        <button class="pro-edit-btn">
+                                            <img
+                                                src="../assets/img/edit.svg"
+                                                alt=""
+                                            />
+                                            Edit
+                                        </button>
+                                    </router-link>
+                                    <button class="pro-delete-btn">
+                                        <img
+                                            src="../assets/img/delete.svg"
+                                            alt=""
+                                        />
+                                        Delete
+                                    </button>
                                 </div>
                             </td>
                         </tr>
@@ -158,14 +224,27 @@
                             <td>$1000</td>
                             <td>
                                 <div class="d-flex align-items-center gap-2">
-                                <router-link :to="{name:'edit',params:{id:'1'}}">
-                                    <button class="pro-edit-btn">
-                                    <img src="../assets/img/edit.svg" alt=""> Edit
-                                </button>
-                                </router-link>
-                                <button class="pro-delete-btn">
-                                    <img src="../assets/img/delete.svg" alt=""> Delete
-                                </button>
+                                    <router-link
+                                        :to="{
+                                            name: 'edit',
+                                            params: { id: '1' },
+                                        }"
+                                    >
+                                        <button class="pro-edit-btn">
+                                            <img
+                                                src="../assets/img/edit.svg"
+                                                alt=""
+                                            />
+                                            Edit
+                                        </button>
+                                    </router-link>
+                                    <button class="pro-delete-btn">
+                                        <img
+                                            src="../assets/img/delete.svg"
+                                            alt=""
+                                        />
+                                        Delete
+                                    </button>
                                 </div>
                             </td>
                         </tr>
@@ -176,17 +255,12 @@
     </section>
     <router-view name="userModal"></router-view>
     <router-view name="edited"></router-view>
-    
 </template>
 
 <script>
-
 export default {
-    name:'User',
-
-}
+    name: "User",
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
